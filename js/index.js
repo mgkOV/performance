639d907626a1bd0743edb8420627d821b5b803f6
mgkOV <myagkov.mww@gmail.com>
2020-04-07 02:09:26 +0300

    delete multimedia script and bable

--- js/index.js
+++ js/index.js
@@ -1,10 +1,7 @@
-window.addEventListener('DOMContentLoaded', () => {
-    const multimediaScript = document.createElement('script');
-    multimediaScript.setAttribute('src','./js/multimedia.js');
-    const buttonsContainer = document.querySelector('.buttons-wrap');
+window.addEventListener("DOMContentLoaded", () => {
+  const buttonsContainer = document.querySelector(".buttons-wrap");
 
-    buttonsContainer.innerHTML = '<button class="button button_yellow" type="button">Да</button>' +
-        '<button class="button">Нет</button>';
-    
-    document.querySelector('.multimedia').appendChild(multimediaScript);
+  buttonsContainer.innerHTML =
+    '<button class="button button_yellow" type="button">Да</button>' +
+    '<button class="button">Нет</button>';
 });
