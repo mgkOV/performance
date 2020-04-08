267a4fab25488177a8a38231b7a8047143a92d19
mgkOV <myagkov.mww@gmail.com>
2020-04-08 13:58:35 +0300

    add gcmq

--- gulpfile.js/tasks/css.js
+++ gulpfile.js/tasks/css.js
@@ -3,15 +3,19 @@ const postcss = require("gulp-postcss");
 const cssnano = require("cssnano");
 const atImport = require("postcss-import");
 const cleanCSS = require("gulp-clean-css");
+const gcmq = require("gulp-group-css-media-queries");
 
 function css() {
   // const modules = [atImport(), cssnano()];
   // const modules = [cssnano()];
 
-  return gulp
-    .src("./src/css/*.css")
-    .pipe(cleanCSS({ level: 0 }))
-    .pipe(gulp.dest(`./css`));
+  return (
+    gulp
+      .src("./src/css/*.css")
+      // .pipe(cleanCSS({ level: 0 }))
+      .pipe(gcmq())
+      .pipe(gulp.dest(`./css`))
+  );
 }
 
 module.exports = gulp.series(css);
