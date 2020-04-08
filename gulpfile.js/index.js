3500fb4ea45eb364e5061266e6a1b5a46b9d8e59
mgkOV <myagkov.mww@gmail.com>
2020-04-08 16:48:06 +0300

    clean function

--- gulpfile.js/index.js
+++ gulpfile.js/index.js
@@ -1,9 +1,22 @@
 const gulp = require("gulp");
+const browserSync = require("browser-sync").create("server");
 
 const css = require("./tasks/css");
 const html = require("./tasks/html");
 const javascript = require("./tasks/javascript");
 
+function watch() {
+  browserSync.init({
+    server: {
+      baseDir: "./"
+    },
+    port: 5000
+  });
+
+  gulp.watch("./src/**/*.js", { ignoreInitial: false }, javascript);
+}
+
 module.exports = {
-  build: gulp.series(javascript, html, css)
+  build: gulp.series(javascript, html, css),
+  watch
 };
--- src/js/main.js
+++ src/js/main.js
@@ -14,69 +14,8 @@
     });
     return t[e].call(n.exports, n, n.exports, o), (n.l = !0), n.exports;
   }
-  (o.e = function (t) {
-    var e = n[t];
-    if (0 === e)
-      return new Promise(function (t) {
-        t();
-      });
-    if (e) return e[2];
-    var i = new Promise(function (i, o) {
-      e = n[t] = [i, o];
-    });
-    e[2] = i;
-    var r = document.getElementsByTagName("head")[0],
-      a = document.createElement("script");
-    (a.type = "text/javascript"),
-      (a.charset = "utf-8"),
-      (a.async = !0),
-      (a.timeout = 12e4),
-      o.nc && a.setAttribute("nonce", o.nc),
-      (a.src =
-        o.p +
-        "" +
-        {
-          0: "9b10f65b0c7e7a1d8693",
-          1: "7641180249a9d7be3cfa"
-        }[t] +
-        ".js");
-    var s = setTimeout(l, 12e4);
-    function l() {
-      (a.onerror = a.onload = null), clearTimeout(s);
-      var e = n[t];
-      0 !== e && (e && e[1](new Error("Loading chunk " + t + " failed.")), (n[t] = void 0));
-    }
-    return (a.onerror = a.onload = l), r.appendChild(a), i;
-  }),
-    (o.m = t),
-    (o.c = i),
-    (o.d = function (t, e, i) {
-      o.o(t, e) ||
-        Object.defineProperty(t, e, {
-          configurable: !1,
-          enumerable: !0,
-          get: i
-        });
-    }),
-    (o.n = function (t) {
-      var e =
-        t && t.__esModule
-          ? function () {
-              return t["default"];
-            }
-          : function () {
-              return t;
-            };
-      return o.d(e, "a", e), e;
-    }),
-    (o.o = function (t, e) {
-      return Object.prototype.hasOwnProperty.call(t, e);
-    }),
-    (o.p = "//an.yandex.ru/partner-code-bundles/6187/"),
-    (o.oe = function (t) {
-      throw (console.error(t), t);
-    }),
-    o((o.s = 271));
+
+  (o.m = t), (o.c = i), (o.p = "//an.yandex.ru/partner-code-bundles/6187/"), o((o.s = 271));
 })([
   function (t, e, i) {
     "use strict";
