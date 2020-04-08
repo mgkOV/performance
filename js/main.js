b1c4d0a10a81cced5bbc1977cb1d082bc3653fe2
mgkOV <myagkov.mww@gmail.com>
2020-04-09 02:24:14 +0300

    clean js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 "use strict";
 
-document.addEventListener("DOMContentLoaded", function () {
+$(document).ready(function () {
   $(".card").each(function (e) {
     if ($(this).hasClass("card_size_s")) {
       $(this).css({ "border-radius": "22px" });
@@ -8,6 +8,7 @@ document.addEventListener("DOMContentLoaded", function () {
       $(this).css({ "border-radius": "23px" });
     }
   });
+
   // var waterContainer = document.querySelector(".card.card_size_s:last-child");
 
   // waterContainer.innerHTML =
@@ -21,60 +22,49 @@ document.addEventListener("DOMContentLoaded", function () {
   //   '<p class="card-source">Чайник</p>' +
   //   '<p class="card-time card-time_block">16:20, Сегодня</p>' +
   //   "</div>";
-});
 
-$(document).ready(function () {
   var carousel = $("#carousel");
-
   carousel.owlCarousel();
+
+  const buttonsContainer = document.querySelector(".buttons-wrap");
+
+  buttonsContainer.innerHTML =
+    '<button class="button button_yellow" type="button">Да</button>' +
+    '<button class="button">Нет</button>';
+
+  const fridgeInfoContainer = document.querySelector(".card_size_m:nth-child(8) .card-description");
+  const confirmPurchaseButton = document.querySelector(".buttons-wrap .button_yellow");
+  const purchaseListContainer = document.createElement("div");
+  const purchaseListTitle = document.createElement("p");
+  const purchaseList = document.createElement("ol");
+  const purchaseListItemOne = document.createElement("li");
+  const purchaseListItemTwo = document.createElement("li");
+
+  purchaseListContainer.setAttribute("class", "purchase-list-wrap");
+  purchaseListTitle.setAttribute(
+    "class",
+    "card-description card-description_big description_critical"
+  );
+  purchaseListTitle.textContent = "Список покупок:";
+  purchaseList.setAttribute("class", "purchase-list");
+  purchaseListItemOne.setAttribute("class", "purchase-list__item");
+  purchaseListItemOne.textContent = "Хлеб";
+  purchaseListItemTwo.setAttribute("class", "purchase-list__item");
+  purchaseListItemTwo.textContent = "Молоко";
+
+  purchaseListContainer.appendChild(purchaseListTitle);
+  purchaseListContainer.appendChild(purchaseList);
+  purchaseList.appendChild(purchaseListItemOne);
+  purchaseList.appendChild(purchaseListItemTwo);
+
+  confirmPurchaseButton.onclick = () => {
+    fridgeInfoContainer.replaceWith(purchaseListContainer);
+    buttonsContainer.style.display = "none";
+  };
+
+  document
+    .getElementsByClassName("header-menu__switcher")[0]
+    .addEventListener("click", function () {
+      document.getElementsByClassName("header-menu")[0].classList.toggle("header-menu_active");
+    });
 });
-
-document.addEventListener(
-  "DOMContentLoaded",
-  function () {
-    const buttonsContainer = document.querySelector(".buttons-wrap");
-
-    buttonsContainer.innerHTML =
-      '<button class="button button_yellow" type="button">Да</button>' +
-      '<button class="button">Нет</button>';
-
-    const fridgeInfoContainer = document.querySelector(
-      ".card_size_m:nth-child(8) .card-description"
-    );
-    const confirmPurchaseButton = document.querySelector(".buttons-wrap .button_yellow");
-    const purchaseListContainer = document.createElement("div");
-    const purchaseListTitle = document.createElement("p");
-    const purchaseList = document.createElement("ol");
-    const purchaseListItemOne = document.createElement("li");
-    const purchaseListItemTwo = document.createElement("li");
-
-    purchaseListContainer.setAttribute("class", "purchase-list-wrap");
-    purchaseListTitle.setAttribute(
-      "class",
-      "card-description card-description_big description_critical"
-    );
-    purchaseListTitle.textContent = "Список покупок:";
-    purchaseList.setAttribute("class", "purchase-list");
-    purchaseListItemOne.setAttribute("class", "purchase-list__item");
-    purchaseListItemOne.textContent = "Хлеб";
-    purchaseListItemTwo.setAttribute("class", "purchase-list__item");
-    purchaseListItemTwo.textContent = "Молоко";
-
-    purchaseListContainer.appendChild(purchaseListTitle);
-    purchaseListContainer.appendChild(purchaseList);
-    purchaseList.appendChild(purchaseListItemOne);
-    purchaseList.appendChild(purchaseListItemTwo);
-
-    confirmPurchaseButton.onclick = () => {
-      fridgeInfoContainer.replaceWith(purchaseListContainer);
-      buttonsContainer.style.display = "none";
-    };
-
-    document
-      .getElementsByClassName("header-menu__switcher")[0]
-      .addEventListener("click", function () {
-        document.getElementsByClassName("header-menu")[0].classList.toggle("header-menu_active");
-      });
-  },
-  !1
-);
