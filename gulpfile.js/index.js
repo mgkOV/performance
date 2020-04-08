8570ca32c2e203f870db3e51d241886ce54e4ff6
mgkOV <myagkov.mww@gmail.com>
2020-04-09 01:51:55 +0300

    delete timeout

---
+++ gulpfile.js/index.js
@@ -0,0 +1,26 @@
+const gulp = require("gulp");
+const browserSync = require("browser-sync").create("server");
+
+const css = require("./tasks/css");
+const html = require("./tasks/html");
+const javascript = require("./tasks/javascript");
+
+function watch() {
+  browserSync.init({
+    server: {
+      baseDir: "./"
+    },
+    port: 5000
+  });
+
+  gulp
+    .watch("./src/**/*.js", { ignoreInitial: false }, javascript)
+    .on("change", browserSync.reload);
+
+  gulp.watch("./src/index.html", { ignoreInitial: false }, html).on("change", browserSync.reload);
+}
+
+module.exports = {
+  build: gulp.series(javascript, html, css),
+  watch
+};
