b34949256776989c0eb878794caeb27b8868db93
mgkOV <myagkov.mww@gmail.com>
2020-04-09 04:38:10 +0300

    rebuild svg

--- gulpfile.js/tasks/svg.js
+++ gulpfile.js/tasks/svg.js
@@ -2,7 +2,21 @@ const gulp = require("gulp");
 const svgmin = require("gulp-svgmin");
 
 function svg() {
-  return gulp.src("./src/img/*.svg").pipe(svgmin()).pipe(gulp.dest(`./img`));
+  return gulp
+    .src("./src/img/*.svg")
+    .pipe(
+      svgmin({
+        js2svg: {
+          pretty: true
+        },
+        plugins: [
+          {
+            removeDoctype: false
+          }
+        ]
+      })
+    )
+    .pipe(gulp.dest(`./img`));
 }
 
 module.exports = gulp.series(svg);
