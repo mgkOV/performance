ed05e77868ce69c05ccd9d923872f619d2e27eed
mgkOV <myagkov.mww@gmail.com>
2020-04-09 06:46:08 +0300

    uglify js

--- gulpfile.js/tasks/javascript.js
+++ gulpfile.js/tasks/javascript.js
@@ -2,15 +2,7 @@ const gulp = require("gulp");
 const uglify = require("gulp-terser");
 
 function javascript() {
-  return gulp
-    .src("./src/js/*.js")
-    .pipe(
-      uglify({
-        keep_fnames: true,
-        mangle: false
-      })
-    )
-    .pipe(gulp.dest(`./js`));
+  return gulp.src("./src/js/*.js").pipe(uglify()).pipe(gulp.dest(`./js`));
 }
 
 module.exports = gulp.series(javascript);
